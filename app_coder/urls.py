59d8f801d6d2cdb3cb468e29155978800b03d84c
Ricardo <rfmaciel.maciel@gmail.com>
2024-09-24 19:31:29 -0300

    principio de herencia

--- app_coder/urls.py
+++ app_coder/urls.py
@@ -3,8 +3,8 @@ from app_coder.views import *
 
 urlpatterns = [
     path('agregar-producto/<nombre>/<ingredientes>/<precio>', crea_producto),
-    path('lista_empanadas/', lista_productos),     
-    path('',inicio),
+    path('lista_empanadas/', lista_productos),   
+    path('',inicio),  
     path('empanada/',empanada, name='empanada'),
     path('pizza/',pizza, name='pizza'),
     path('hamburguesa/',hamburguesa, name='hamburguesa'),
